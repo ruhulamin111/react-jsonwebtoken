61e761b81238a88dfdae7441f018a4a69fd86709
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-12 20:08:05 +0600

    post method

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -2,10 +2,17 @@ import React from 'react';
 import auth from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Button } from '@material-tailwind/react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const LogIn = () => {
-    const [signInWithGoogle] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location?.state?.from?.pathname || '/';
+    if (user) {
+        navigate(from, { replace: true })
+    }
     const handleGoogle = () => {
         signInWithGoogle()
     }
