f505aca784ed01284dbeaf8edcc730ab55602d72
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-12 14:15:27 +0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddProduct from './components/AddProduct/AddProduct';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import LogIn from './components/LogIn/LogIn';
+import OrderList from './components/OrderList/OrderList';
+import Products from './components/Products/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orderlist' element={<OrderList></OrderList>}></Route>
+        <Route path='/addproduct' element={<AddProduct></AddProduct>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderList = () => {
+    return (
+        <div>
+            <h2>order list</h2>
+        </div>
+    );
+};
+
+export default OrderList;
